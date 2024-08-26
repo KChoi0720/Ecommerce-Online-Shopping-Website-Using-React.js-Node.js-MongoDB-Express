638f8311d0fa7543eca56111b69e5ee7500d9544
KevinC <choicanlight@gmail.com>
2024-08-26 19:47:04 +1200

    # shopcontext modification

--- frontend/src/contexts/ShopContext.jsx
+++ frontend/src/contexts/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 
@@ -10,11 +10,38 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
-    const [showSearch, setShowSearch] = useState(false)
+    const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]){
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
+
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart
     }
 
     return (
