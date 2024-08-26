c39b971f3df1e505905f1d0e56f3e5b309e34ad9
KevinC <choicanlight@gmail.com>
2024-08-26 21:29:29 +1200

    # add relatedproduct

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -21,9 +21,21 @@ const RelatedProducts = ({category, subCategory}) => {
 
   return (
     <div>
-      
+      <div className='text-center text-3xl py-2'>
+        <Title text1={'RELATED'} text2={'PRODUCTS'}/>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap'>
+        {realted.map((item, index)=>(
+            <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+        ))}
+      </div>
     </div>
   )
 }
 
 export default RelatedProducts
+
+
+
+
