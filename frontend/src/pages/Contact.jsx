3b578792b547eff23ca22d68bfe859efff21bb95
KevinC <choicanlight@gmail.com>
2024-08-27 23:40:39 +1200

    # update placeorder login cart about contact order

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const Contact = () => {
+  return (
+    <div>
+      
+      <div className='text-center text-2xl pt-10 border-t'>
+          <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+          <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt=""/>
+          <div className='flex flex-col items-start gap-6'>
+            <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+            <p className='text-gray-500'>1234 Dominion Road <br /> Auckland, NewZealand, 1024</p>
+            <p className='text-gray-500'>Tel: (027) 555-1234 <br /> Email: admin@magic.co.nz</p>
+            <p className='font-semibold text-gray-600'>Careers at Magic</p>
+            <p className='text-gray-500'>Learn more about our teams and job vacancies.</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          </div>
+      </div>
+      
+      <NewsletterBox />
+
+    </div>
+  )
+}
+
+export default Contact
