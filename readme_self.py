3b8c5d3f0055a90d02f29d57aad0086db4e90357
KevinC <choicanlight@gmail.com>
2024-09-15 00:34:49 +1200

    Add files via upload

---
+++ readme_self.py
@@ -0,0 +1,16 @@
+# npm create vite@latest
+# > npx
+# > create vite
+# frontend
+# React
+# Javascript
+# open frontend folder
+# cd frontend
+# npm install
+# npm install react-router-dom react-toastify # dom for multiple routes, toastify for notifications
+# npm run dev
+# add extension ES7+React/Redux/React-Native snippets.
+
+# google: vite + tailwind
+# install tailwind css : npm install -D tailwindcss postcss autoprefixer
+# install tailwind css : 
