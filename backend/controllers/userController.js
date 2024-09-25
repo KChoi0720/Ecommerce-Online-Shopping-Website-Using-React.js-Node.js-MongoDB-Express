f97d55e50bbcc0161ad17663ba0074f75a5924be
KevinC <choicanlight@gmail.com>
2024-09-25 23:47:53 +1200

    update usercontroller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+import userModel from "../models/userModel.js"
+
 
 // Route for user login
 const loginUser = async (req, res) => {
@@ -10,7 +12,23 @@ const loginUser = async (req, res) => {
 // Route for user registration
 const registerUser = async (req, res) => {
 
-    res.json({msg: "Register API Working"})
+    try {
+
+        const {name, email, password} = req.body;
+
+        // checking user already exists or not
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false, message:"User already exists"})
+        }
+
+        
+        
+    } catch (error) {
+        
+    }
+
+    // res.json({msg: "Register API Working"})
 
 }
 
