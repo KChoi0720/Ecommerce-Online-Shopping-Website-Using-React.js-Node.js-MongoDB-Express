2b26b5a25c364305f15deb87cc6ad6141532dd13
KevinC <choicanlight@gmail.com>
2024-09-26 22:22:00 +1200

    #update controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,12 @@
+import validator from "validator"
+import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
 import userModel from "../models/userModel.js"
+
+
+const createToken = (id) => {
+    return  jwt.sign({id},process.env.JWT_SECRET)
+}
 
 
 // Route for user login
@@ -22,10 +30,35 @@ const registerUser = async (req, res) => {
             return res.json({success:false, message:"User already exists"})
         }
 
+        // validating email format &strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message:"Please enter a valid email"})
+            
+        }
+        if (password.length < 8) {
+            return res.json({success:false, message:"Please enter a strong password"})
+            
+        }
+
+        // hasing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
         
+        const user = await newUser.save()
+        
+        const token = createToken(user._id)
+
+        res.json({success:true, token})
         
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false, message:error.message})
     }
 
     // res.json({msg: "Register API Working"})
