6eae2e87eda73e9b8e14727a00a4154c147865d8
KevinC <choicanlight@gmail.com>
2024-09-30 21:04:58 +1300

    generate productController and test in ThunderClient

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,31 +1,49 @@
-
-
 // function for add product
 const addProduct = async (req, res) => {
-
-
-
-}
-
-// function for list product
-const listProducts = async (req, res) => {
-
-
-    
-}
-
-// function for remove product
-const removeProduct = async (req, res) => {
-
-
-    
-}
-
-// function for single product
-const singleProduct = async (req, res) => {
-
-
-    
-}
-
-export {listProducts, addProduct, removeProduct, singleProduct}
+    try {
+      const {
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller,
+      } = req.body;
+  
+      const image1 = req.files.image1[0];
+      const image2 = req.files.iamge2[0];
+      const image3 = req.files.image3[0];
+      const image4 = req.files.image4[0];
+  
+      console.log(
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller
+      );
+  
+      console.log(image1, image2, image3, image4);
+  
+      res.json({})
+  
+    } catch (error) {
+      console.log(error)
+      res.json({success:false, message:error.message})
+    }
+  };
+  
+  // function for list product
+  const listProducts = async (req, res) => {};
+  
+  // function for remove product
+  const removeProduct = async (req, res) => {};
+  
+  // function for single product
+  const singleProduct = async (req, res) => {};
+  
+  export { listProducts, addProduct, removeProduct, singleProduct };
+  
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,28 @@
 import express from "express";
+import multer from "multer"
 import {
   listProducts,
   addProduct,
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
+
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Set your upload folder path
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // Generate unique file names
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 
 const productRouter = express.Router();
 
