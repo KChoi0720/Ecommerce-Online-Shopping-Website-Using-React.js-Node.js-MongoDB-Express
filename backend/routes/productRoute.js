870559d177a872285f49d774f326a3b57a137df3
KevinC <choicanlight@gmail.com>
2024-10-06 23:33:21 +1300

    # create adminAuth

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,34 +1,18 @@
 import express from "express";
-import multer from "multer"
 import {
   listProducts,
   addProduct,
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js"
 
-
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Set your upload folder path
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    // Generate unique file names
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 
 const productRouter = express.Router();
 
-productRouter.post(
-  "/add",
-  upload.fields([
+productRouter.post("/add", adminAuth, upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
     { name: "image3", maxCount: 1 },
@@ -36,7 +20,7 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post("/remove", removeProduct);
+productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProducts);
 
